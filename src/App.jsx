eb4d1c51aa06aadb19a63b72836b602a29a5badf
eamrandoc <eamrandoc@gmail.comm>
2025-03-01 23:23:05 +0600

    first set up

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-9xl">THis Is Bangladesh</h1>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar/Navbar";
+
+
+const MainLayout = () => {
+    return (
+        <div className="bg-gray-400">
+            This is Main Layout
+            <Navbar></Navbar>       
+        </div>
+    );
+};
+
+export default MainLayout;
